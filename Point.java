7724ac84312378afb57d6ea962fb0bb3b4bb2be5
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-12-21 22:18:45 +0530

    Added the point class.

---
+++ Point.java
@@ -0,0 +1,15 @@
+public class Point{
+
+	double x, y;
+
+	Point(double ix, double iy){
+		x = ix;
+		y = iy;
+	}
+
+	Point(){ }
+
+	public double getX(){ return x; }
+	public double getY(){ return y; }
+
+}
